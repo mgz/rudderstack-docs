15dac765a74f02dd3d684cf6b7f7b38d5a7f177a
Ramendra Malviya <ramendra.malviya@redcrackle.com>
2021-04-13 15:16:17 +0530

    fixed nav bar design

--- src/components/middle-banner.js
+++ src/components/middle-banner.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../lib/font-awesome';
+import { StaticImage } from "gatsby-plugin-image"
+
     
 const MiddleBanner = (props) => {
     const smalldescription = props.smalldescription
@@ -12,17 +14,19 @@ const MiddleBanner = (props) => {
               <div className="container md:px-3 mx-auto">
                     <div className="flex flex-wrap grid justify-items-center md:grid-cols-12 items-center sm:flex-row">	
                         <div className="w-full flex items-start md:col-span-5 md:px-20 p-6 mt-6">
-                            <FontAwesomeIcon icon="arrow-right" pull="left" className="text-blue font-normal" size="6x" />
-                            <h3 className="my-4 text-4xl md:text-5xl font-bold leading-tight">{title}</h3>
+                            <div className="mt-3">
+                                <StaticImage src="../images/Arrow.svg" alt="Get Started"/>
+                            </div>
+                            <h3 className="text-4xl ml-5 md:text-5xl font-bold font-custom leading-tight">{title}</h3>
                         </div>
                         <div className="w-full md:col-span-7 md:px-20 p-6 mt-6">
-                            <div className="align-middle text-lg leading-7">
+                            <div className="align-middle text-lg leading-7 font-custom">
                                 <p>{smalldescription}</p>
                                 <div className="block md:flex pt-4">
                                 {
                                     btns.map(
                                         (btn, i) => (
-                                            <a key={i} className={(btn.btnhiglight === true ? 'bg-white text-primary' : '') + ` py-2 text-sm md:px-8 lg:px-8 px-2 text-center mr-4 rounded-lg md:mb-0 mb-4 md:flex block border leading-tight border-white`} href={btn.btnlink}>{btn.btntext}</a>
+                                            <a key={i} className={(btn.btnhiglight === true ? 'bg-white text-primary' : '') + ` py-2 text-sm md:px-8 lg:px-8 px-2 text-center mr-4 rounded-lg md:mb-0 mb-4 md:flex block border leading-tight border-white font-custom`} href={btn.btnlink}>{btn.btntext}</a>
                                         )
                                     )
                                 }
