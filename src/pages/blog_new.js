8c57bd11197fb4d1b64fee6b8e1faa06ead082ee
Red Crackle <sathish2006cbe@gmail.com>
2021-04-15 19:05:33 +0530

    pushed about us page changes

--- src/pages/blog_new.js
+++ src/pages/blog_new.js
@@ -9,8 +9,8 @@ import CustomMenu from "../components/blogCategoryList";
 import { InstantSearch, Configure, SortBy, Menu } from 'react-instantsearch-dom';
 import CustomHits from "../components/customHits"
 
-const Blog = (props) => {
-  const searchClient = algoliasearch('CXEE6UEOI8', '88f9637ee245ea4c30cfa44892b3b5ec');
+const Blog_new = (props) => {
+  const searchClient = algoliasearch(process.env.RS_GATSBY_ALGOLIA_APP_ID, process.env.RS_GATSBY_ALGOLIA_APIKEY);
   const { data } = props
   const pageInfo = data.allSanityBlog.pageInfo
   return (
@@ -18,7 +18,7 @@ const Blog = (props) => {
       <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
         <InstantSearch
           searchClient={searchClient}
-          indexName='rudderstack_blog'
+          indexName={process.env.RS_GATSBY_ALGOLIA_BLOGINDEX}
           >
           <Configure hitsPerPage={13} />
           <div className="container flex flex-wrap flex-col sm:flex-row">
@@ -48,7 +48,7 @@ const Blog = (props) => {
   )
 }
 
-export default Blog
+export default Blog_new
 
 
 export const pageQuery = graphql`
