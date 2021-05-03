7f9f0bfce28bda69c3b9f43fda51b72917b8ba4a
hariomshah <hariom23shah@gmail.com>
2021-05-03 23:01:46 +0530

    nav mobile styling fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -118,7 +118,7 @@ const Layout = ({ children }) => {
                           href="https://resources.rudderstack.com/join-rudderstack-slack"
                           target="_blank"
                           rel="noreferrer"
-                          className="w-full md:w-auto font-bold py-3 my-6 btn-secondary-lg sm:btn-primary-lg md:mb-0 text-sm lg:px-4 px-2 text-center lg:mr-4 rounded-lg mb-4 inline-block border leading-tight border-white font-custom"
+                          className="w-full md:w-auto font-bold py-3 my-6 btn-primary-lg md:mb-0 text-sm lg:px-4 px-2 text-center lg:mr-4 rounded-lg mb-4 inline-block border leading-tight border-white font-custom"
                         >
                           <StaticImage
                             src="../images/Slack.svg"
@@ -131,7 +131,7 @@ const Layout = ({ children }) => {
                           href="https://docs.rudderstack.com"
                           target="_blank"
                           rel="noreferrer"
-                          className="w-full md:w-auto font-bold py-3 my-6 btn-secondary-lg sm:btn-primary-lg md:mb-0 text-sm md:px-8 lg:px-8 px-2 text-center md:mr-4 rounded-lg mb-4 inline-block border leading-tight border-white font-custom"
+                          className="w-full md:w-auto font-bold py-3 my-6 btn-primary-lg md:mb-0 text-sm md:px-8 lg:px-8 px-2 text-center md:mr-4 rounded-lg mb-4 inline-block border leading-tight border-white font-custom"
                         >
                           Go to docs
                         </a>
