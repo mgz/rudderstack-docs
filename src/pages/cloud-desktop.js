713c543dcb114f5c7cc8d71d91d8978007ea82e5
Monika Pathania <m.pathania@thisisdmg.com>
2021-05-18 14:07:32 +0530

    added comment

--- src/pages/cloud-desktop.js
+++ src/pages/cloud-desktop.js
@@ -30,7 +30,6 @@ const CloudDesktop = ({ data }) => {
 
   return (
     <Layout>
-
       <section className="w-full product-page_banner text-white lg:py-0 py-10 flex justify-center items-center relative overflow-hidden">
         <div className="max-w-6xl sm:pb-20 pb-4 p1-16 px-4 md:px-3 mx-auto">
           <div className="flex flex-col items-center justify-center mx-auto lg:flex-row lg:p-0">
@@ -44,22 +43,19 @@ const CloudDesktop = ({ data }) => {
                 <a class="btn-secondary-lg sm:mr-4 md:mb-0 mb-4" href="/request-demo">Get a demo</a>
               </div>
             </div>
-
             <div className="relative w-full px-5 rounded-lg flex-grow justify-items-end lg:w-2/5 sm:px-0 sm:items-center lg:items-start lg:mb-0">
               <div class="relative rounded-md sm:px-0 items-center flex justify-center bg-white product-video">
                 <img src={ProductVideo} alt="Product Video" className="w-full" />
                 <div className="absolute left-0 right-0 mx-auto z-0">
-                  <img src={VideoChart} alt="Product Video" className="w-100 p-20 mx-auto"/>
+                  <img src={VideoChart} alt="Product Video" className="w-100 p-20 mx-auto" />
                 </div>
                 <button className="absolute left-0 right-0 mx-auto" type="button"><img src={VideoPlayBtn} alt="Product Video" className="w-full" /></button>
-
               </div>
             </div>
           </div>
         </div>
-      </section>
-
-      <section>
+      </section>{/*banner*/}
+      <section>{/*Advantage*/}
         <div className="w-full">
           <div className="pb-0 pt-12 max-w-6xl mx-auto px-4 md:px-3 lg:pt-20 flex flex-col justify-center text-center">
             <div class="sm:mb-24 mb-10"><span class="text-3xl md:text-5xl font-bold">The RudderStack Advantage</span></div>
@@ -92,17 +88,13 @@ const CloudDesktop = ({ data }) => {
                     <p>Stop paying your CDP a premium to store your data. RudderStack’s warehouse-first architecture is efficient, because we don’t have to own storage of your data. This means we don’t need to build and maintain components most products do - like a big, elastic database - so we don’t have to charge for them.</p>
                     <p className="mt-5">RudderStack is free up to 500,000 events and our usage-based pricing scales transparently from there.</p>
                   </div>
-
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
-
-
-
-      <section className="bg-backgroundsecondary 100% left-right-section sm:py-24 py-11">
+      </section>{/*Advantage*/}
+      <section className="bg-backgroundsecondary 100% left-right-section sm:py-24 py-11">{/*Collect your data*/}
         <div className="max-w-6xl px-4 md:px-3 mx-auto">
           <div className="flex flex-col w-full justify-center items-center">
             <h3 className="md:mt-2 text-4xl md:text-5xl text-primary font-bold leading-tight text-center max-w-screen-sm">
@@ -112,7 +104,7 @@ const CloudDesktop = ({ data }) => {
               Collect your data
             </h3>
           </div>
-          <div className="flex items-center flex-wrap sm:flex-row mt-6 md:mt-16">
+          <div className="flex items-center flex-wrap sm:flex-row mt-6 md:mt-16">{/*Event Stream*/}
             <div className="sm:w-1/2 sm:p-6 sm:pl-0 md:pr-20">
               <h3 className="mt-2 mb-4 pb-2 text-2xl md:text-3xl text-primary font-bold leading-tight">
                 Event stream
@@ -137,9 +129,7 @@ const CloudDesktop = ({ data }) => {
               <img src={EventStream} alt="event stream" className="w-full" />
             </div>
           </div>
-
-
-          <div className="flex items-center flex-wrap flex-col-reverse sm:flex-row mt-6 md:mt-16">
+          <div className="flex items-center flex-wrap flex-col-reverse sm:flex-row mt-6 md:mt-16">{/*Cloud Extract*/}
             <div className="w-full sm:w-1/2 sm:p-6 sm:pl-0 mt-12 sm:mt-0">
               <img src={CloudExtract} alt="cloud extract" className="w-full" />
             </div>
@@ -156,14 +146,13 @@ const CloudDesktop = ({ data }) => {
                 </span>
               </div>
             </div>
-
           </div>
           <div className="flex flex-col w-full justify-center items-center">
             <h3 className="mt-24 text-4xl md:text-5xl text-blue font-bold leading-tight text-center">
               Put your data to work
             </h3>
           </div>
-          <div className="flex items-center flex-wrap sm:flex-row mt-6 md:mt-16">
+          <div className="flex items-center flex-wrap sm:flex-row mt-6 md:mt-16">{/*Warehouse Actions*/}
             <div className="sm:w-1/2 sm:p-6 sm:pl-0 md:pr-20">
               <h3 className="mt-2 mb-4 pb-2 text-2xl md:text-3xl text-primary font-bold leading-tight">
                 Warehouse Actions
@@ -196,9 +185,7 @@ const CloudDesktop = ({ data }) => {
               RudderStack helps you achieve a seamless data workflow
               </h3>
           </div>
-
-          <div className="max-w-5xl px-0 md:px-3 mx-auto">
-
+          <div className="max-w-5xl px-0 md:px-3 mx-auto">{/*Data WorkFlow*/}
             <div className="flex flex-wrap flex-row">
               <div className="w-full sm:w-1/2 sm:px-5 mt-10">
                 <div className="h-full flex flex-col justify-start items-center text-center bg-white py-10 px-8 rounded-2xl">
@@ -212,10 +199,8 @@ const CloudDesktop = ({ data }) => {
                       <a href="https://rudderstack.com/integration/">More from Rudderstack docs</a>
                     </span>
                   </div>
-
                 </div>
               </div>
-
               <div className="w-full sm:w-1/2 sm:px-5 mt-10">
                 <div className="h-full flex flex-col justify-start items-center text-center bg-white py-10 px-8 rounded-2xl">
                   <div className="h-20">
@@ -228,7 +213,6 @@ const CloudDesktop = ({ data }) => {
                       <a href="https://rudderstack.com/integration/">More from Rudderstack docs</a>
                     </span>
                   </div>
-
                 </div>
               </div>
               <div className="w-full sm:w-1/2 sm:px-5 mt-10">
@@ -243,10 +227,8 @@ const CloudDesktop = ({ data }) => {
                       <a href="https://rudderstack.com/integration/">More from Rudderstack docs</a>
                     </span>
                   </div>
-
                 </div>
               </div>
-
               <div className="w-full sm:w-1/2 sm:px-5 mt-10">
                 <div className="h-full flex flex-col justify-start items-center text-center bg-white py-10 px-8 rounded-2xl">
                   <div className="h-20">
@@ -263,14 +245,7 @@ const CloudDesktop = ({ data }) => {
                 </div>
               </div>
             </div>
-
-          </div>
-
-
-
-
-
-
+          </div>{/*Data WorkFlow*/}
         </div>
       </section>
       <section className="100% bg-whiteColor-custom">
@@ -293,73 +268,7 @@ const CloudDesktop = ({ data }) => {
           </div>
         </div>
       </section>
-      {/* <section className="100% bg-whiteColor-custom">
-        <div className="max-w-6xl px-4 md:px-3 mx-auto">
-          <div className="flex flex-col sm:flex-row items-center md:gap-12 justify-center mx-auto lg:p-0 md:mt-11 ">
-            <div className="w-full sm:w-3/5 sm:p-6 sm:pl-0 mt-12 sm:mt-0">
-              <h2 className="text-3xl-4 md:text-5xl text-primary font-bold leading-tight">
-                Integrate your entire stack
-            </h2>
-              <p className="text-secondary leading-7 text-sm sm:text-lg py-6">Rudderstack supports 100+ out of the box integrations, and we add more every month. Connect your customer data to all the tools your team uses to drive growth. Spend less time on vendor SDK implementations and third-party code management.</p>
-              <div class="flex items-center">
-                <span class="font-bold leading-normal text-sm lr-icon relative inline-flex sm:inline-block hover:text-blueNew-custom">
-                  <a href="https://rudderstack.com/integration/">See all integrations</a>
-                </span>
-              </div>
-            </div>
-            <div className="w-full sm:w-2/5 sm:p-6 sm:pr-0 list-stream">
-              <img src={Integrationschart} alt="product" className="w-full" />
-            </div>
-          </div>
-        </div>
-
-        <div className="max-w-6xl px-4 md:px-3 mx-auto">
-          <div className="flex flex-col-reverse sm:flex-row items-center md:gap-12 justify-center mx-auto lg:p-0 md:mt-11 ">
-            <div className="w-full sm:w-2/5 sm:p-6 sm:pr-0 list-stream">
-              <img src={DriveChart} alt="product" className="w-full" />
-            </div>
-            <div className="w-full sm:w-3/5 sm:p-6 sm:pl-0 mt-12 sm:mt-0">
-              <h2 className="text-3xl-4 md:text-5xl text-primary font-bold leading-tight">
-                Let your customer data drive your business
-            </h2>
-              <p className="text-secondary leading-7 text-sm sm:text-lg py-6">Events are a fine-grained description of a customer’s behavior. Event Stream identifies users across devices and delivers events in real-time to the software and infrastructure your teams use, like marketing automation platforms, product analytics tools, and internal streaming infrastructure.</p>
-              <p className="text-secondary leading-7 text-sm sm:text-lg">Your teams can use your event data in these tools for analysis - like building more specific audiences - or they can even automate the execution of tactics - like behavior-triggered emails.</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="max-w-6xl px-4 md:px-3 mx-auto">
-          <div className="flex flex-col sm:flex-row items-center md:gap-12 justify-center mx-auto lg:p-0 md:mt-11 ">
-            <div className="w-full sm:w-3/5 sm:p-6 sm:pl-0 mt-12 sm:mt-0">
-              <h2 className="text-3xl-4 md:text-5xl text-primary font-bold leading-tight">
-                Event streaming for your entire customer data stack
-            </h2>
-              <p className="text-secondary leading-7 text-sm sm:text-lg py-6">Event Stream gives your teams the flexibility to choose the tools they want to use without worrying about how to connect your customer data.</p>
-              <p className="text-secondary leading-7 text-sm sm:text-lg">Use RudderStack’s SDKs to instrument the front-end and back-end of your websites and mobile applications and stream events to 100+ destinations and your data warehouse.</p>
-            </div>
-            <div className="w-full sm:w-2/5 sm:p-6 sm:pr-0 list-stream">
-              <img src={StreamData} alt="product" className="w-full" />
-            </div>
-          </div>
-        </div>
-
-        <div className="max-w-6xl px-4 md:px-3 mx-auto">
-          <div className="flex flex-col-reverse sm:flex-row items-center md:gap-12 justify-center mx-auto lg:p-0 mb-10 md:mt-11">
-            <div className="w-full sm:w-2/5 sm:p-6 sm:pr-0 list-stream">
-              <img src={DataControl} alt="product" className="w-full" />
-            </div>
-            <div className="w-full sm:w-3/5 sm:p-6 sm:pl-0 mt-12 sm:mt-0">
-              <h2 className="text-3xl-4 md:text-5xl text-primary font-bold leading-tight">
-                Keep control of your customer data
-            </h2>
-              <p className="text-secondary leading-7 text-sm sm:text-lg py-6">RudderStack’s warehouse-first architecture builds your customer data lake and your identity graph on your data warehouse, not our infrastructure. We don’t persist any of the data you send through RudderStack.</p>
-              <p className="text-secondary leading-7 text-sm sm:text-lg">So you don’t have to rely on us to protect your sensitive data. It’s in your data warehouse, and you have control of its security and privacy.</p>
-            </div>
-          </div>
-        </div>
-
-      </section>  */}
-      <section className="overflow-hidden">
+      <section className="overflow-hidden">{/*Join Our Team*/}
         <div className="testimonials bg-white px-7 py-10 sm:py-20 -mx-2.5 text-primary margin-top-xl bg-hero-pattern-mobile sm:bg-hero-pattern bg-no-repeat bg-cover">
           <div className="w-full text-center">
             <h2 className="text-4xl md:text-5xl font-bold mb-6">
