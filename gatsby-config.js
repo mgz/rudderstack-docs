e6a206ca8f61ebb4ebe4bb4c24e2031036872392
hariomshah <hariom23shah@gmail.com>
2021-07-01 14:04:16 +0530

    optimization

--- gatsby-config.js
+++ gatsby-config.js
@@ -170,6 +170,16 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `limelight`,
+          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
+        ],
+        display: "swap",
+      },
+    },
+    {
       resolve: `gatsby-plugin-feed`,
       options: {
         query: `
