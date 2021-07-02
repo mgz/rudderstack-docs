70cd5a103c6dc569494e89ece1eaea71ebd91dfd
kilajyot <jyothi.kilaru@redcrackle.com>
2021-07-02 23:04:42 +0530

    Updated chillipiper script

--- src/templates/request-demo.js
+++ src/templates/request-demo.js
@@ -275,12 +275,12 @@ const Demo = ({ data, htmlId, location }) => {
       <Helmet>
         <script>
           {`function q(a){return function(){ChiliPiper[a].q=(ChiliPiper[a].q||[]).concat([arguments])}}window.ChiliPiper=window.ChiliPiper||"submit scheduling showCalendar submit widget bookMeeting".split(" ").reduce(function(a,b){a[b]=q(b);return a},{});
-ChiliPiper.scheduling("rudderstack-main-gatsby-website.s3-website-us-east-1.amazonaws.com", "request_demo_form_top", {title: "Thanks! What time works best for a quick call?"})
+ChiliPiper.scheduling("rudderstack", "demo-or-quote-request", {title: "Thanks! What time works best for a quick call?"})
 `}
         </script>
         <script
           src="https://js.na.chilipiper.com/marketing.js"
-          type="text/javascript"
+          type="text/javascript" async
         />
       </Helmet>
     </Layout>
