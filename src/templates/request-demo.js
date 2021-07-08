f6b2986d9c8d1f5c0d69b114f10a76bbacfe7ea4
hariomshah <hariom23shah@gmail.com>
2021-07-08 23:23:46 +0530

    chilli piper changes

--- src/templates/request-demo.js
+++ src/templates/request-demo.js
@@ -163,11 +163,11 @@ const Demo = ({ data, htmlId, location }) => {
           // console.log('usebasin',res)
           if (res.ok || res.statusText === "OK") {
             // console.log("success ", res)
-            // window.ChiliPiper.scheduling(
-            //   "rudderstack",
-            //   "demo-or-quote-request",
-            //   { formId: data.formId }
-            // )
+            window.ChiliPiper.scheduling(
+              "rudderstack",
+              "demo-or-quote-request",
+              { formId: data.formId }
+            )
             // navigate("/request-demo/thank-you")
           }
         })
@@ -293,7 +293,7 @@ const Demo = ({ data, htmlId, location }) => {
 ChiliPiper.scheduling("rudderstack", "demo-or-quote-request", {title: "Thanks! What time works best for a quick call?"})
 `}
         </script> */}
-        <script>
+        {/* <script>
           {`
           function q(a){return function(){ChiliPiper[a].q=(ChiliPiper[a].q||[]).concat([arguments])}}window.ChiliPiper=window.ChiliPiper||"submit scheduling showCalendar submit widget bookMeeting".split(" ").reduce(function(a,b){a[b]=q(b);return a},{});
           ChiliPiper.scheduling("rudderstack", "demo-or-quote-request", {formId: "request_demo_form_top"})
@@ -304,7 +304,7 @@ ChiliPiper.scheduling("rudderstack", "demo-or-quote-request", {title: "Thanks! W
           function q(a){return function(){ChiliPiper[a].q=(ChiliPiper[a].q||[]).concat([arguments])}}window.ChiliPiper=window.ChiliPiper||"submit scheduling showCalendar submit widget bookMeeting".split(" ").reduce(function(a,b){a[b]=q(b);return a},{});
           ChiliPiper.scheduling("rudderstack", "demo-or-quote-request", {formId: "request_demo_form_bottom"})
         `}
-        </script>
+        </script> */}
         <script
           src="https://js.na.chilipiper.com/marketing.js"
           type="text/javascript"
