9acf4747ab9ff8fff37e72e2c804bcd4a590c4c8
kilajyot <jyothi.kilaru@redcrackle.com>
2021-08-23 09:25:21 +0530

    Added purgecss:'

--- gatsby-config.js
+++ gatsby-config.js
@@ -229,10 +229,10 @@ module.exports = {
         // develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+        purgeOnly : ['/'], // Purge only these files/folders
         purgeCSSOptions: {
           // https://purgecss.com/configuration.html#options
-          // safelist: ['safelist'], // Don't remove this selector
+           safelist: ['gradient-enable'], // Don't remove this selector
         },
       },
    },
