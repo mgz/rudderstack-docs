c213a82d92a8ee680ab19d71d2451f37907abcd6
Gaurav Prabhu <gauravprabhu15@gmail.com>
2021-09-10 19:07:03 +0530

    Fixed accordion bugs

--- src/components/tempDataCheck.js
+++ src/components/tempDataCheck.js
@@ -1,192 +1,111 @@
 import React from "react"
 
+/* tempJson{
+  slug: "/home",
+  title: "Home",
+  url: "#home"
+} */
+
 /* function getSearchData(data) {
   console.log("Data", data)
-  let tempArr = [] //final data to be passed to Algolia
-  let tempJson = {}
-  if (i.items == undefined) {
-    tempJson["title"] = i.title
-    tempJson["section"] = i.slug + i.url
-    tempArr.push(tempJson)
+  if (data.items === undefined) {
+    tempJson["title"] = data.title
+    tempJson["section"] = data.url
+    console.log("Temp Arr", tempJson)
   } else {
-    i.items.map((item, k) => {
-      let childTempJson = getSearchData(item)
-      tempArr.push(childTempJson)
+    data.items.map((item, k) => {
+      getSearchData(item)
     })
   }
   return tempArr
 } */
 
+function makeSectionUrl(str) {
+  str = str.toLowerCase()
+  str =
+    "#" +
+    str
+      .replace("/", "")
+      .replace("?", "")
+      .replace(".", "")
+      .replace(" ", "-")
+      .replace("  ", "--")
+  return str
+}
+
+let tempArr = [] //final data to be passed to Algolia
+function getSearchData(data) {
+  let elSlug = data.node.slug
+  data.node.headings.map((i, k) => {
+    let tempJson = {}
+    tempJson["slug"] = elSlug + makeSectionUrl(i.value)
+    tempJson["title"] = i.value
+    tempArr.push(tempJson)
+    console.log("Temp Json", tempArr)
+    return tempArr
+  })
+}
+
 const tempJsonData = [
   {
     node: {
-      slug: "glossary",
-      tableOfContents: {
-        items: [
-          {
-            url: "#glossary",
-            title: "Glossary",
-            items: [
-              {
-                url: "#general",
-                title: "General",
-                items: [
-                  {
-                    url: "#customer-data-platform",
-                    title: "Customer Data Platform",
-                  },
-                  {
-                    url: "#etl-extract-transform-load",
-                    title: "ETL (Extract, Transform, Load)",
-                  },
-                  {
-                    url: "#reverse-etl",
-                    title: "Reverse-ETL",
-                  },
-                ],
-              },
-              {
-                url: "#core-product",
-                title: "Core Product",
-                items: [
-                  {
-                    url: "#control-plane",
-                    title: "Control Plane",
-                  },
-                  {
-                    url: "#data-plane",
-                    title: "Data Plane",
-                  },
-                  {
-                    url: "#config-generator",
-                    title: "Config Generator",
-                  },
-                ],
-              },
-              {
-                url: "#features",
-                title: "Features",
-                items: [
-                  {
-                    url: "#event-stream",
-                    title: "Event Stream",
-                  },
-                  {
-                    url: "#cloud-extract",
-                    title: "Cloud Extract",
-                  },
-                  {
-                    url: "#warehouse-actions",
-                    title: "Warehouse Actions",
-                  },
-                  {
-                    url: "#transformations",
-                    title: "Transformations",
-                  },
-                  {
-                    url: "#live-events",
-                    title: "Live Events",
-                  },
-                  {
-                    url: "#data-governance",
-                    title: "Data Governance",
-                  },
-                  {
-                    url: "#user-suppression",
-                    title: "User Suppression",
-                  },
-                ],
-              },
-              {
-                url: "#integrations",
-                title: "Integrations",
-                items: [
-                  {
-                    url: "#connection-pipeline",
-                    title: "Connection (Pipeline)",
-                  },
-                  {
-                    url: "#connection-modes-cloud-mode--device-mode",
-                    title: "Connection Modes (Cloud Mode & Device Mode)",
-                  },
-                  {
-                    url: "#source",
-                    title: "Source",
-                  },
-                  {
-                    url: "#destination",
-                    title: "Destination",
-                  },
-                  {
-                    url: "#warehouse-destination",
-                    title: "Warehouse Destination",
-                  },
-                ],
-              },
-              {
-                url: "#other-terminology",
-                title: "Other Terminology",
-                items: [
-                  {
-                    url: "#api-specification",
-                    title: "API Specification",
-                  },
-                  {
-                    url: "#sdk",
-                    title: "SDK",
-                  },
-                  {
-                    url: "#warehouse-schema",
-                    title: "Warehouse Schema",
-                  },
-                  {
-                    url: "#workspace-token",
-                    title: "Workspace Token",
-                  },
-                  {
-                    url: "#write-key",
-                    title: "Write Key",
-                  },
-                ],
-              },
-              {
-                url: "#contact-us",
-                title: "Contact Us",
-              },
-            ],
-          },
-        ],
+      slug: "faqs",
+      headings: [
+        {
+          value: "Installation and Setup",
+        },
+        {
+          value: "RudderStack Server",
+        },
+        {
+          value: "Config Generator",
+        },
+        {
+          value: "Transformations",
+        },
+        {
+          value: "RudderStack Cloud",
+        },
+        {
+          value: "Integrations",
+        },
+        {
+          value: "RudderStack Pro / Enterprise",
+        },
+        {
+          value: "RudderStack Failover, Hardening and Security",
+        },
+        {
+          value: "Retry Behavior",
+        },
+        {
+          value: "Throttling Behavior",
+        },
+        {
+          value: "Contact Us",
+        },
+      ],
+      frontmatter: {
+        title: "FAQs - RudderStack",
       },
     },
   },
   {
     node: {
-      slug: "",
-      tableOfContents: {
-        items: [
-          {
-            url: "#home",
-            title: "Home",
-            items: [
-              {
-                url: "#quick-start",
-                title: "Quick Start",
-              },
-              {
-                url: "#license",
-                title: "License",
-              },
-              {
-                url: "#contribute",
-                title: "Contribute",
-              },
-              {
-                url: "#follow-us",
-                title: "Follow Us",
-              },
-            ],
-          },
-        ],
+      slug: "cloud-extract-sources/activecampaign",
+      headings: [
+        {
+          value: "Getting Started",
+        },
+        {
+          value: "FAQs",
+        },
+        {
+          value: "Contact Us",
+        },
+      ],
+      frontmatter: {
+        title: "ActiveCampaign - RudderStack",
       },
     },
   },
@@ -199,7 +118,8 @@ export const TempButton = () => {
         console.log(
           "on map ",
           tempJsonData.map((i, k) => {
-            //let menuItem = getSearchData(i.node.tableOfContents)
+            let menuItem = getSearchData(i)
+            return menuItem
           })
         )
       }
