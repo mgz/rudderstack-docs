42fb41f66ef78dba24149d430cfb9d15a04db6a2
hariomshah <hariom23shah@gmail.com>
2021-10-22 14:35:33 +0530

    changes

--- src/components/heroBannerCaseStudies.js
+++ src/components/heroBannerCaseStudies.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react"
 
 const HeroBannerCaseStudies = () => {
-    return <div className="hero-case-studies py-48"> Hero Banner Case Studies</div>
+  return (
+    <div className="hero-case-studies">
+      <div className="hero-case-studies-bg-image py-48">Hero Banner Case Studies</div>
+    </div>
+  )
 }
 
 export default HeroBannerCaseStudies
