4a944a6bd224d9bf9d4ed01754249afff6983dc4
hariomshah <hariom23shah@gmail.com>
2021-11-11 17:51:36 +0530

    wip

---
+++ src/components/testimonialCardWithLeftHeading.js
@@ -0,0 +1,10 @@
+import React from "react"
+import PortableText from "./portableText"
+import Image from "./image"
+
+const TestimonialCardWithLeftHeading = props => {
+  console.log("TestimonialCardWithLeftHeading", props)
+  return <div className="max-w-6xl px-0 md:px-3 mx-auto">some text</div>
+}
+
+export default TestimonialCardWithLeftHeading
