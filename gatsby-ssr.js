ad83f3c77602a70bba13c8084daedc37e9c16944
kilajyot <jyothi.kilaru@redcrackle.com>
2021-11-28 00:00:29 +0530

    inline to link

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -56,6 +56,18 @@ export const onPreRenderHTML = ({
 }) => {
   const headComponents = getHeadComponents()
 
+  headComponents.forEach(element => {
+    if (element.type === "style" && element.props["data-href"]) {
+      element.type = "link"
+      element.props.href = element.props["data-href"]
+      element.props.rel = "stylesheet"
+      element.props.type = "text/css"
+
+      delete element.props["data-href"]
+      delete element.props.dangerouslySetInnerHTML
+    }
+  })
+  
   headComponents.sort((x, y) => {
     if (x.props && x.props["data-react-helmet"]) {
       return -1
