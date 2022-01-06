6aced22daf568e9a5e853f58a17b8f35d152bc0c
kilajyot <jyothi.kilaru@redcrackle.com>
2022-01-07 00:40:00 +0530

    fixed typo

--- src/components/IntegrationSimilarV2.js
+++ src/components/IntegrationSimilarV2.js
@@ -28,7 +28,7 @@ const IntegrationSimilarV2 = ({ data }) => {
     <section className="100% bg-whiteColor-custom">
       <div className="max-w-6xl px-4 md:px-3 mx-auto flex flex-wrap pb-24">
         <h3 className="w-full my-2 text-4xl md:text-5xl text-dark font-bold leading-tight mb-8 md:mb-20 mt-6 md:mt-28">
-          Similer Integrations
+          Similar Integrations
         </h3>
         <div className="w-full grid grid-cols-1 sm:grid-cols-3 gap-4 md:gap-8">
           {eligibleNodes &&
