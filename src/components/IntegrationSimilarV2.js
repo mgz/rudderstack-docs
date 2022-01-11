3cbf916add9b7f4ef91834bb13d7a148e4af7d57
Gaurav Prabhu <gauravprabhu15@gmail.com>
2022-01-11 20:20:21 +0530

    Hide load more button on complete integrations load

--- src/components/IntegrationSimilarV2.js
+++ src/components/IntegrationSimilarV2.js
@@ -85,7 +85,7 @@ const IntegrationSimilarV2 = ({ data }) => {
             })}
         </div>
 
-        <div className="w-full my-2 mt-12 text-blueNew-magenta flex justify-center">
+        {visibleIntegrationCount <= eligibleNodes.length && (<div className="w-full my-2 mt-12 text-blueNew-magenta flex justify-center">
           <p
             className="btn-primary-lg text-darkScheme-textBlack hover:text-darkScheme-btnPrimaryBg w-64 cursor-pointer"
             onClick={() => {
@@ -94,7 +94,7 @@ const IntegrationSimilarV2 = ({ data }) => {
           >
             Load more
           </p>
-        </div>
+        </div>)}
       </div>
     </section>
   )
