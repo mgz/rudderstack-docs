eff07224700d5354c78499d86c00851ed9e28c6a
Shalineen Rathore <shalineenrathore@Shalineens-iMac.local>
2022-02-09 13:21:29 +0530

    shalinee | added comparison table of competitor

--- src/components/InfoLanding/CompetitorComparison/CompetitorComparison.js
+++ src/components/InfoLanding/CompetitorComparison/CompetitorComparison.js
@@ -14,30 +14,30 @@ const CompetitorComparison = props => {
                 <h1 className="landing-section-title">RudderStack vs. Snowplow</h1>
 
                 <div className="hidden lg:block mt-14 lg:px-4 px-6">
-                    <table className="pricing-table relative sm:w-full table-fixed font-custom text-grayColor-custom">
+                    <table className={`${s.competitor_comparison_table}  pricing-table relative sm:w-full table-fixed font-custom text-grayColor-custom `}>
                         <thead>
-                            <tr  className="grad-bg rounded-2xl"  >
-                                <th className="z-10 sticky-pricing-banner triggers" colSpan="2"  >                                    
+                            <tr className="grad-bg rounded-2xl">
+                                <th className="z-10 sticky-pricing-banner triggers w-1/3"   >
                                 </th>
-                                <th className="z-10 sticky-pricing-banner triggers rounded-tl-2xl rounded-bl-2xl">
+                                <th className="z-10 sticky-pricing-banner triggers rounded-tl-2xl rounded-bl-2xl w-1/3">
                                     <div className="ellipse flex items-center justify-center h-20">
                                         <StaticImage src="../../../images/rudder-stack-logo.svg" />
                                     </div>
-                                    
+
                                 </th>
-                                <th className="z-10 sticky-pricing-banner triggers rounded-br-2xl rounded-tr-2xl">
+                                <th className="z-10 sticky-pricing-banner triggers rounded-br-2xl rounded-tr-2xl w-1/3">
                                     <div className="ellipse flex items-center justify-center h-20">
                                         <StaticImage src="../../../images/snowplow.svg" />
                                     </div>
-                                    
+
                                 </th>
-                        </tr>
+                            </tr>
                         </thead>
                         <tbody>
                             <tr className="triggers">
                                 <td colSpan={"4"}>
                                     <table className="w-full">
-                                        <tbody> 
+                                        <tbody>
 
                                             <tr>
                                                 <td className="p-6 text-darkScheme-btnSecondaryBg text-2xl text-left uppercase font-bold" colSpan={"2"}>
@@ -46,8 +46,8 @@ const CompetitorComparison = props => {
                                             </tr>
 
                                             <tr
-                                                className="grad-bg rounded-2xl"  >
-                                                <td className="p-6 text-lg text-left w-1/4 rounded-tl-2xl rounded-bl-2xl text-darkScheme-textPrimary opacity-50">
+                                                className="grad-bg rounded-2xl">
+                                                <td className="p-6 text-lg text-left w-1/4 rounded-tl-2xl rounded-bl-2xl text-darkScheme-textPrimary">
                                                     Event Replay functionality
                                                 </td>
 
@@ -60,7 +60,7 @@ const CompetitorComparison = props => {
                                                 </td>
 
                                                 <td className="text-base py-4 px-12 w-1/4 rounded-br-2xl rounded-tr-2xl">
-                                                <span className={`check-icon check-comparison`} />
+                                                    <span className={`check-icon check-comparison`} />
                                                     <span className="font-normal text-darkScheme-textPrimary">
 
                                                     </span>
@@ -70,8 +70,8 @@ const CompetitorComparison = props => {
                                             </tr>
 
                                             <tr
-                                                className="bg-transparent"  >
-                                                <td className="p-6 text-lg text-left w-1/4 rounded-tl-2xl rounded-bl-2xl text-darkScheme-textPrimary opacity-50">
+                                                className="bg-transparent">
+                                                <td className="p-6 text-lg text-left w-1/4 rounded-tl-2xl rounded-bl-2xl text-darkScheme-textPrimary">
                                                     Open-source offering
                                                 </td>
 
@@ -84,7 +84,7 @@ const CompetitorComparison = props => {
                                                 </td>
 
                                                 <td className="text-base py-4 px-12 w-1/4 rounded-br-2xl rounded-tr-2xl">
-                                                <span className={`check-icon check-comparison`} />
+                                                    <span className={`check-icon check-comparison`} />
                                                     <span className="font-normal text-darkScheme-textPrimary">
 
                                                     </span>
@@ -92,8 +92,8 @@ const CompetitorComparison = props => {
 
                                             </tr>
                                             <tr
-                                                className="grad-bg rounded-2xl"  >
-                                                <td className="p-6 text-lg text-left w-1/4 rounded-tl-2xl rounded-bl-2xl text-darkScheme-textPrimary opacity-50">
+                                                className="grad-bg rounded-2xl">
+                                                <td className="p-6 text-lg text-left w-1/4 rounded-tl-2xl rounded-bl-2xl text-darkScheme-textPrimary">
                                                     Ad-blocker resillient
                                                 </td>
 
@@ -106,7 +106,7 @@ const CompetitorComparison = props => {
                                                 </td>
 
                                                 <td className="text-base py-4 px-12 w-1/4 rounded-br-2xl rounded-tr-2xl">
-                                                <span className={`check-icon check-comparison`} />
+                                                    <span className={`check-icon check-comparison`} />
                                                     <span className="font-normal text-darkScheme-textPrimary">
 
                                                     </span>
@@ -115,8 +115,8 @@ const CompetitorComparison = props => {
                                             </tr>
 
                                             <tr
-                                                className="bg-transparent"  >
-                                                <td className="p-6 text-lg text-left w-1/4 rounded-tl-2xl rounded-bl-2xl text-darkScheme-textPrimary opacity-50">
+                                                className="bg-transparent">
+                                                <td className="p-6 text-lg text-left w-1/4 rounded-tl-2xl rounded-bl-2xl text-darkScheme-textPrimary">
                                                     Real-time event and custom transformations
                                                 </td>
 
@@ -137,8 +137,8 @@ const CompetitorComparison = props => {
 
                                             </tr>
                                             <tr
-                                                className="grad-bg rounded-2xl"  >
-                                                <td className="p-6 text-lg text-left w-1/4 rounded-tl-2xl rounded-bl-2xl text-darkScheme-textPrimary opacity-50">
+                                                className="grad-bg rounded-2xl">
+                                                <td className="p-6 text-lg text-left w-1/4 rounded-tl-2xl rounded-bl-2xl text-darkScheme-textPrimary">
                                                     Event Schema Inference
                                                 </td>
 
@@ -160,41 +160,41 @@ const CompetitorComparison = props => {
                                             </tr>
 
                                             <tr
-                                                className="bg-transparent"  >
-                                                <td className="p-6 text-lg text-left w-1/4 rounded-tl-2xl rounded-bl-2xl text-darkScheme-textPrimary opacity-50">
+                                                className="bg-transparent">
+                                                <td className="p-6 text-lg text-left w-1/4 rounded-tl-2xl rounded-bl-2xl text-darkScheme-textPrimary">
                                                     Data governance and schema enforcement
                                                 </td>
 
                                                 <td className="text-base py-4 px-12 w-1/4">
-                                                     
+
                                                     <span className="font-normal text-darkScheme-textPrimary">
-                                                    API based
+                                                        API based
                                                     </span>
 
                                                 </td>
 
-                                                <td className="text-base py-4 px-12 w-1/4"> 
+                                                <td className="text-base py-4 px-12 w-1/4">
                                                     <span className="font-normal text-darkScheme-textPrimary">
-                                                    UI based
+                                                        UI based
                                                     </span>
                                                 </td>
 
                                             </tr>
 
                                             <tr
-                                                className="grad-bg rounded-2xl"  >
-                                                <td className="p-6 text-lg text-left w-1/4 rounded-tl-2xl rounded-bl-2xl text-darkScheme-textPrimary opacity-50">
+                                                className="grad-bg rounded-2xl">
+                                                <td className="p-6 text-lg text-left w-1/4 rounded-tl-2xl rounded-bl-2xl text-darkScheme-textPrimary">
                                                     Data ownership
                                                 </td>
 
-                                                <td className="text-base py-4 px-12 w-1/4"> 
+                                                <td className="text-base py-4 px-12 w-1/4">
                                                     <span className="font-normal text-darkScheme-textPrimary">
                                                         Doesn’t store your data
                                                     </span>
 
                                                 </td>
 
-                                                <td className="text-base py-4 px-12 w-1/4 rounded-br-2xl rounded-tr-2xl"> 
+                                                <td className="text-base py-4 px-12 w-1/4 rounded-br-2xl rounded-tr-2xl">
                                                     <span className="font-normal text-darkScheme-textPrimary">
                                                         Keeps a copy of your data
                                                     </span>
@@ -202,13 +202,13 @@ const CompetitorComparison = props => {
 
                                             </tr>
                                             <tr>
-                                                <td className="p-6 text-darkScheme-btnSecondaryBg text-2xl text-left uppercase font-bold" colSpan={"2"}>
+                                                <td className="p-6 pt-9 text-darkScheme-btnSecondaryBg text-2xl text-left uppercase font-bold" colSpan={"2"}>
                                                     SDKS, SOURCES AND SUPPORTED DESTINATIONS
                                                 </td>
                                             </tr>
                                             <tr
-                                                className="grad-bg rounded-2xl"  >
-                                                <td className="p-6 text-lg text-left w-1/4 rounded-tl-2xl rounded-bl-2xl text-darkScheme-textPrimary opacity-50">
+                                                className="grad-bg rounded-2xl">
+                                                <td className="p-6 text-lg text-left w-1/4 rounded-tl-2xl rounded-bl-2xl text-darkScheme-textPrimary">
                                                     Web / mobile / server-side event tracking
                                                 </td>
 
@@ -221,15 +221,12 @@ const CompetitorComparison = props => {
                                                 </td>
 
                                                 <td className="text-base py-4 px-12 w-1/4  rounded-br-2xl rounded-tr-2xl">
-                                                <span className={`check-icon check-comparison`} />
+                                                    <span className={`check-icon check-comparison`} />
                                                     <span className="font-normal text-darkScheme-textPrimary">
 
                                                     </span>
                                                 </td>
-
                                             </tr>
-
-
                                         </tbody>
                                     </table>
                                 </td>
@@ -238,10 +235,73 @@ const CompetitorComparison = props => {
 
                         </tbody>
                     </table>
- 
+
                 </div>
 
-
+                <div className="mobile-table font-custom w-full">
+                    <div className="group-name text-darkScheme-btnSecondaryBg text-lg uppercase font-bold my-7">CORE FEATURES</div>
+                   
+                    <div className={`${s.freeplan_wrapper}`}>
+                        <div className="freeplan lg:mt-11 mt-4 sticky sticky-pricing-banner flex grad-bg">
+                            <div className="pricing-wrapper w-1/2" >
+                                <div className="plan-header">
+                                    <div className="p-6 md:pt-4 md:pb-4 pt-2 pb-2">
+                                        <div className="text-grayColor-lighter leading-6 text-sm py-2  flex items-center">
+                                            <StaticImage src="../../../images/rudder-stack-logo.svg" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="pricing-wrapper w-1/2" >
+                                <div className="plan-header">
+                                    <div className="p-6 md:pt-4 md:pb-4 pt-2 pb-2">
+                                        <div className="text-grayColor-lighter leading-6 text-sm py-2  flex items-center">
+                                            <StaticImage src="../../../images/snowplow.svg" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                   
+                    <div className="plan-features">
+                        <div className="pricing-block" >
+                            <div className={`pricing-row py-4 pb-2 px-2 `}>
+                                <div className="text-darkScheme-textPrimary">Event Replay functionality</div>
+                                <div className="pricing-values flex">
+                                    <div className="w-1/2 self-center h-14 flex items-center justify-center">
+                                        <span className={`check-icon check-comparison`} />
+                                    </div>
+                                    <div className="w-1/2 self-center h-14 flex items-center justify-center">
+                                        <span className={`line-image`} />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={`pricing-row py-4 pb-2 px-2 grad-bg`}>
+                                <div className="text-darkScheme-textPrimary">150+ Cloud Destinations  (GA, Amplitude, Braze, etc.)</div>
+                                <div className="pricing-values flex">
+                                    <div className="w-1/2 self-center h-14 flex items-center justify-center">
+                                        <span className={`check-icon check-comparison`} />
+                                    </div>
+                                    <div className="w-1/2 self-center h-14 flex items-center justify-center">
+                                        <span className={`line-image`} />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={`pricing-row py-4 pb-2 px-2`}>
+                                <div className="text-darkScheme-textPrimary">Open-source offering</div>
+                                <div className="pricing-values flex">
+                                    <div className="w-1/2 self-center h-14 flex items-center justify-center">
+                                        <span className={`check-icon check-comparison`} />
+                                    </div>
+                                    <div className="w-1/2 self-center h-14 flex items-center justify-center">
+                                        <span className={`line-image`} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
