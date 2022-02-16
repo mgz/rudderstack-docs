169124325004a6eb2c2d7db42321642576765ec2
Gaurav Prabhu <gauravprabhu@Gauravs-MacBook-Pro.local>
2022-02-16 16:42:38 +0530

    Bug fix

--- src/components/SignupShortForm.js
+++ src/components/SignupShortForm.js
@@ -107,7 +107,7 @@ const SignupShortForm = props => {
                   }}
                 />
                 <button
-                  className="font-bold btn-primary-lg text-center signup-short-button w-full md:w-auto"
+                  className="font-bold btn-primary-lg text-center signup-short-button w-full md:w-auto px-6"
                   type={"submit"}
                   onClick={e => formSubmitted(e)}
                 >
