106b59ac0be7c087e9b0c8aa5a4b9b3897924fe3
Shalineen Rathore <shalineenrathore@Shalineens-iMac.local>
2022-02-25 18:10:40 +0530

    shalinee | changed text class for mobile view

--- src/components/AnnounceBanner.js
+++ src/components/AnnounceBanner.js
@@ -8,7 +8,7 @@ const AnnounceBanner = ({announcebannerbuttons}) => {
                 {announcebannerbuttons && announcebannerbuttons.map((btn, i) => {
                     return (
                     <Link to={btn.btnlink} className="learn-more text-darkScheme-textPrimary font-bold inline-flex relative d-flex items-center" key={btn._key}>
-                        <span className='learn-more-text inline-block md:text-2xl text-lg'>{btn.btntext}</span>
+                        <span className='learn-more-text inline-block md:text-2xl text-sm'>{btn.btntext}</span>
                         <span className="learn-more-icon lrlm-icon text-darkScheme-btnSecondaryBg inline-block pl-5"><svg width="14" height="23" viewBox="0 0 8 11" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L7 5.5L1 10" stroke="#86F8E0" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg></span>
                     </Link>)
                 })}
